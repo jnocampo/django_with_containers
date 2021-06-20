4e02d848c06b142eadc7ac8e540185175ca1ea2e
Kiyoshi Hashida <kiyoshihashida@kiyoshis-mbp.lan>
2021-06-20 12:06:32 -1000

    Added html pages(classes,supplies,textbook,dorm,foodie), updated views.py and urls.py

--- greetings/urls.py
+++ greetings/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('new', views.CreateView.as_view(), name='create'),
     path('update/<int:pk>', views.UpdateView.as_view(), name='update'),
     path('delete/<int:pk>', views.DeleteView.as_view(), name='delete'),
+    path('textbook/', views.TextbookView.as_view(), name='textbook'),
+    path('dorm/', views.DormView.as_view(), name='dorm'),
+    path('classes/', views.ClassesView.as_view(), name='classes'),
+    path('foodie/', views.FoodieView.as_view(), name='foodie'),
+    path('supplies/', views.SuppliesView.as_view(), name='supplies'),
 ]
--- greetings/views.py
+++ greetings/views.py
@@ -26,3 +26,23 @@ class DeleteView(generic.edit.DeleteView):
     template_name = 'greetings/delete.html' # override default of greetings/greeting_confirm_delete.html
     model = Greeting
     success_url = reverse_lazy('greetings:index')
+
+class TextbookView(generic.ListView):
+    template_name = 'greetings/textbook.html'
+    model = Greeting
+
+class DormView(generic.ListView):
+    template_name = 'greetings/dorm.html'
+    model = Greeting
+
+class ClassesView(generic.ListView):
+    template_name = 'greetings/classes.html'
+    model = Greeting
+
+class FoodieView(generic.ListView):
+    template_name = 'greetings/Foodie.html'
+    model = Greeting
+
+class SuppliesView(generic.ListView):
+    template_name = 'greetings/Supplies.html'
+    model = Greeting
